90089887d450664b641917d4310954682447530a
James Call <jacall2012@byui.edu>
2024-01-14 21:57:51 -0700

    added some get functions, dynamicaly generate new column names

--- main.py
+++ main.py
@@ -4,22 +4,34 @@ from utilities import AnalysisUtilities
 
 def main():
     # Specify the path to your Excel file
-    file_path = 'LC2-032_KCP1 pHL-YEMK DC 20231030.xlsx'
+    file_path = AnalysisUtilities.getfile_path()
+    file_path = "root/" + file_path 
+    renamed_column_names_list = AnalysisUtilities.get_renamed_column_names()
+    new_column_names_list = AnalysisUtilities.get_new_column_names()
+    sheet1_name = AnalysisUtilities.getsheet1_name()
+    sheet2_name = AnalysisUtilities.getsheet2_name()
+    new_sheet_name = AnalysisUtilities.get_new_sheet_name()
 
     # Prepare analysis DataFrame
-    analysis_df = AnalysisUtilities.prepare_analysis_df(file_path)
+    combined_df = AnalysisUtilities.prepare_analysis_df(file_path, sheet1_name, sheet2_name)
+
+    old_column_name_list = AnalysisUtilities.get_old_column_names(combined_df)
+
+    analysis_df = AnalysisUtilities.rewrite_column_names(combined_df, renamed_column_names_list, new_column_names_list)
 
     # Calculate 'pHL_VL2_BL1' column
-    analysis_df = AnalysisUtilities.calculate_pHL_VL2_BL1(analysis_df)
+    #analysis_df = AnalysisUtilities.calculate_pHL_VL2_BL1(analysis_df)
 
     # Calculate 'yemk_vl2_bl1' column
-    analysis_df = AnalysisUtilities.calculate_yemk_vl2_bl1(analysis_df)
+    #analysis_df = AnalysisUtilities.calculate_yemk_vl2_bl1(analysis_df)
 
     # Calculate 'relative_well_number' column
-    analysis_df = AnalysisUtilities.calculate_relative_well_number(analysis_df)
+    #analysis_df = AnalysisUtilities.calculate_relative_well_number(analysis_df)
+
+    #slope_phl_vl2_phl_bl1 = AnalysisUtilities.calculate_slope(analysis_df, AnalysisUtilities.get_slope_X_column_name(), AnalysisUtilities.get_slope_Y_Column_name())
 
     # Write analysis sheet
-    AnalysisUtilities.write_analysis_sheet(analysis_df, file_path)
+    #AnalysisUtilities.write_analysis_sheet(analysis_df, file_path, new_sheet_name)
 
 if __name__ == "__main__":
     main()
--- utilities.py
+++ utilities.py
@@ -2,11 +2,51 @@ import pandas as pd
 from scipy.stats import linregress
 
 class AnalysisUtilities:
+    
     @staticmethod
-    def prepare_analysis_df(file_path):
+    def getfile_path():
+        return 'LC2-032_KCP1 pHL-YEMK DC 20231030.xlsx'
+
+    @staticmethod
+    def getsheet1_name():
+        return "Samples"
+
+    @staticmethod
+    def getsheet2_name():
+        return "High Controls"
+    
+    @staticmethod
+    def get_new_sheet_name():
+        return "Analysis"
+
+    @staticmethod
+    def get_old_column_names(combined_df):
+        # Extract only the column names from the combined_df DataFrame
+        old_column_name_list = combined_df.columns.tolist()
+
+        print(old_column_name_list)
+        
+        return old_column_name_list
+
+    @staticmethod
+    def get_new_column_names():
+
+        new_column_names_list = ["pHL_VL2_BL1","yemk_vl2_bl1","relative_well_number","slope_corrected_phl_vl2_bl1","slope_corrected_yemk_vl2_bl1","cutoff_PHL_VL2_BL1_below_cuttoff","cutoff_yemk_vl2_bl1_below_cuttoff","phl_z_score","yemk_z_score","live_z_score","hits_phl_z_score","hits_yemk_z_score","hits_live_z_score"]
+
+        return new_column_names_list 
+
+    @staticmethod
+    def get_renamed_column_names():
+        
+        renamed_column_names_list = ["well_number","total_count","phl_count","yemk_count","live_percentage","dead_percentage","phl_vl2","phl_bl1","yemk_vl2","yemk_bl1"]
+
+        return renamed_column_names_list
+
+    @staticmethod
+    def prepare_analysis_df(file_path, sheet1, sheet2):
         # Create DataFrames for "Samples" and "High Controls" sheets
-        samples_df = pd.read_excel(file_path, sheet_name='Samples')
-        high_controls_df = pd.read_excel(file_path, sheet_name='High Controls')
+        samples_df = pd.read_excel(file_path, sheet_name=sheet1)
+        high_controls_df = pd.read_excel(file_path, sheet_name=sheet2)
 
         # Combine the DataFrames into one
         combined_df = pd.concat([samples_df, high_controls_df], ignore_index=True)
@@ -15,41 +55,27 @@ class AnalysisUtilities:
         combined_df = combined_df.dropna(how='all')
 
         # Convert values in "X1" column to lowercase and remove rows where the value is "mean" or "sd"
-        combined_df = combined_df[~combined_df['X1'].str.lower().isin(['mean', 'sd'])]
+        combined_df = combined_df[~combined_df[combined_df.columns[0]].str.lower().isin(['mean', 'sd'])]
+
+        return combined_df
+
+    @staticmethod
+    def rewrite_column_names(combined_df, renamed_column_names_list, new_column_names_list):
+        analysis_df = combined_df.copy()  # Create a copy to avoid modifying the original DataFrame
 
         # Rename existing columns
-        analysis_df = combined_df.rename(columns={
-            'X1': 'well_number',
-            'Count': 'total_count',
-            'Live/Cells/Singlet.Cells/pHL.|.Count': 'phl_count',
-            'Live/Cells/Singlet.Cells/YEMK.|.Count': 'yemk_count',
-            'Live.|.Freq..of.Total.(%)': 'live_percentage',
-            'Dead.|.Freq..of.Total.(%)': 'dead_percentage',
-            'Live/Cells/Singlet.Cells/pHL.|.Median.(VL2-H.::.VL2-H)': 'phl_vl2',
-            'Live/Cells/Singlet.Cells/pHL.|.Median.(BL1-H.::.BL1-H)': 'phl_bl1',
-            'Live/Cells/Singlet.Cells/YEMK.|.Median.(VL2-H.::.VL2-H)': 'yemk_vl2',
-            'Live/Cells/Singlet.Cells/YEMK.|.Median.(BL1-H.::.BL1-H)': 'yemk_bl1'
-        })
+        for old_name, new_name in zip(renamed_column_names_list, new_column_names_list):
+            analysis_df.rename(columns={old_name: new_name}, inplace=True)
 
         # Add new empty columns
-        analysis_df['pHL_VL2_BL1'] = ''
-        analysis_df['yemk_vl2_bl1'] = ''
-        analysis_df['relative_well_number'] = ''
-        analysis_df['slope_corrected_phl_vl2_bl1'] = ''
-        analysis_df['slope_corrected_yemk_vl2_bl1'] = ''
-        analysis_df['cutoff_PHL_VL2_BL1_below_cuttoff'] = ''
-        analysis_df['cutoff_yemk_vl2_bl1_below_cuttoff'] = ''
-        analysis_df['phl_z_score'] = ''
-        analysis_df['yemk_z_score'] = ''
-        analysis_df['live_z_score'] = ''
-        analysis_df['hits_phl_z_score'] = ''
-        analysis_df['hits_yemk_z_score'] = ''
-        analysis_df['hits_live_z_score'] = ''
+        for new_name in new_column_names_list:
+            analysis_df[new_name] = ''
 
         return analysis_df
 
+
     @staticmethod
-    def write_analysis_sheet(analysis_df, file_path):
+    def write_analysis_sheet(analysis_df, file_path, new_sheet_name):
         # Check if the "Analysis" sheet already exists and delete it
         with pd.ExcelWriter(file_path, engine='openpyxl', mode='a') as writer:
             if 'Analysis' in writer.sheets:
@@ -101,7 +127,7 @@ class AnalysisUtilities:
         return analysis_df
     
     @staticmethod
-    def calculate_slope_phl_vl2_phl_bl1(analysis_df):
+    def calculate_slope(analysis_df, x_column, y_column):
         """
         Calculate the slope of 'pHL_VL2_BL1' vs 'relative_well_number' columns.
 
@@ -114,10 +140,7 @@ class AnalysisUtilities:
         # Perform linear regression
         slope, _, _, _, _ = linregress(analysis_df['relative_well_number'], analysis_df['pHL_VL2_BL1'])
 
-        # Save the slope as a variable
-        slope_phl_vl2_phl_bl1 = slope
-
-        return slope_phl_vl2_phl_bl1
+        return slope
 
     @staticmethod
     def calculate_slope_corrected_phl_vl2_bl1(analysis_df, slope_phl_vl2_phl_bl1):
@@ -133,3 +156,11 @@ class AnalysisUtilities:
         """
         analysis_df['slope_corrected_phl_vl2_bl1'] = analysis_df['pHL_VL2_BL1'] - (analysis_df['relative_well_number'] * slope_phl_vl2_phl_bl1)
         return analysis_df
+    
+    @staticmethod
+    def get_slope_X_column_name():
+        pass
+
+    @staticmethod
+    def get_slope_Y_Column_name():
+        pass
